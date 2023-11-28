a5855b3acc08c438335a706e6e67cc328b18e68c
Aasthasree <sreelakshmi7032@gmail.com>
2023-11-28 09:31:15 +0530

    feat(core): update registration form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit } from '@angular/core';
-import { FormGroup,FormBuilder,FormArray} from '@angular/forms';
+import { FormGroup,FormBuilder,FormArray,FormControl,Validators} from '@angular/forms';
+
 
 
 @Component({
@@ -11,39 +12,52 @@ export class AppComponent implements OnInit{
   title = 'main-project';
   regform: FormGroup; 
   addressesAreSame:boolean=false
+
   
 
   constructor(private fb: FormBuilder){}
+
+  get skillControls(){
+    return (<FormArray>this.regform.get('skills')).controls;
+  }
 
   
 
   ngOnInit(){
     this.regform = this.fb.group({
-      firstname: [''],
-      lastname: [''],
-      email: [''],
-      date: [''],
-      phone:[''],
-      gender: [''],
+      firstname: ['',Validators.required],
+      lastname: ['',Validators.required],
+      email: ['',[Validators.required,Validators.email]],
+      date: ['',Validators.required],
+      phone:['',Validators.required],
+      gender: ['',Validators.required],
 
       permanent_address: this.fb.group({
-        streetaddress: [''],
-        country: [''],
-        city:[''],
-        region: [''],
-        postalcode: [''],
+        Street: ['',Validators.required],
+        Country: ['',Validators.required],
+        City:['',Validators.required],
+        Region: ['',Validators.required],
+        Postal_code: ['',Validators.required],
       }),
 
+     
+
       communication_address: this.fb.group({
-        streetaddress: [''],
-        country: [''],
-        city:[''],
-        region: [''],
-        postalcode: [''],
+        Street: ['',Validators.required],
+        Country1: ['',Validators.required],
+        City:['',Validators.required],
+        Region: ['',Validators.required],
+        Postal_code: ['',Validators.required],
       }),
-      skills:this.fb.array([
-        this.fb.control('')
-      ])
+
+      skills:this.fb.array(
+        [
+          this.fb.control('')
+        ]
+      )
+    
+
+
       })
 
      
@@ -51,11 +65,44 @@ export class AppComponent implements OnInit{
   }
 
   onSubmit(){
-    if(this.addressesAreSame){
-      const permanent_address  = this.regform.get('permanent_address')?.value
-      this.regform.get('communication_address')?.setValue(permanent_address)
+    const formdata={
+      First_name:this.regform.value.firstname,
+      Last_name:this.regform.value.lastname,
+      Email:this.regform.value.email,
+      DOB:this.regform.value.date,
+      Ph_num:this.regform.value.phone,
+      Gender:this.regform.value.gender,
+      permanent_address:this.regform.value.permanent_address,
+      communication_address: this.addressesAreSame? this.regform.value.permanent_address:this.regform.value.communication_address
+      
     }
-
+    console.log(formdata)
   }
+
+ 
+
+  get skills()
+  {
+      return this.regform.get('skills') as FormArray
+  }
+  addSKill()
+  {
+      this.skills.push( this.fb.control(''))
+      
+  }
+  delete(index:number)
+  {
+      this.skills.removeAt(index);
+  }
+
+  
+
+  
+ 
+ 
+
+  
+
+
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxMaskModule } from 'ngx-mask';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
