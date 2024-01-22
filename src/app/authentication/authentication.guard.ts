c7776866641d23db073d1f3e7cc7c24ffb889fb8
Aasthasree <sreelakshmi7032@gmail.com>
2024-01-22 21:38:03 +0530

    refactor(auth-service): update authentication guard

--- src/app/authentication/authentication.guard.ts
+++ src/app/authentication/authentication.guard.ts
@@ -2,19 +2,20 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthenticationService } from './authentication.service';
 
-
 export const authGuard: CanActivateFn = (route, state) => {
-
-
   const authService = inject(AuthenticationService);
   const router = inject(Router);
 
-  if (authService.isAuthenticated) {
+  if (authService.isAuthenticatedUser()) {
+    // User is authenticated, allow access
     return true;
   }
 
-  // If not authenticated, redirect to the login page
+  // User is not authenticated, redirect to the login page
   router.navigate(['/login']);
   return false;
+}
 
-};
+
+
+
