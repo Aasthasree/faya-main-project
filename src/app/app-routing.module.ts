5c9da52ed61cd06a58e59ff0beeb110244783078
Aasthasree <sreelakshmi7032@gmail.com>
2024-01-22 11:01:51 +0530

    feat(routing-module): add new route to render login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 //component
 import { RegistrationComponent } from './registration/registration.component';
 import { CustomerListComponent } from './customer/customer-list/customer-list.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
   {path: '',component: RegistrationComponent},
-  {path: 'customer-service',component: CustomerListComponent}
+  {path: 'customer-service',component: CustomerListComponent},
+  {path: 'login',component: LoginComponent}
 ];
 
 @NgModule({
