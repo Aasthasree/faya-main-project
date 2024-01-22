9269516fe2174161dc9a985e58b10baf7044a804
Aasthasree <sreelakshmi7032@gmail.com>
2024-01-22 11:37:13 +0530

    feat(admin-module): import components

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
