48316a5bc7972d3afef880a6632c438c1327e565
Aasthasree <sreelakshmi7032@gmail.com>
2024-01-22 15:30:14 +0530

    refactor(admin-routing): add routes

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class AdminRoutingModule { }
