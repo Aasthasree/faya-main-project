98005af8780ecfd8c0a549ef685a5e9edbcde65c
Aasthasree <sreelakshmi7032@gmail.com>
2024-01-23 16:38:04 +0530

    refactor(login-service): rename service file and organize

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { AuthenticationService } from '../authentication/authentication.service';
+import { AuthenticationService } from './service/authentication.service';
 import { Router } from '@angular/router';
 
 @Component({
