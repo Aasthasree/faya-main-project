97c5fd09827a769f8956832ed6559b2c9ebacad0
Aasthasree <sreelakshmi7032@gmail.com>
2024-01-23 16:19:16 +0530

    feat(navbar): add logout method

--- src/app/admin/components/navbar/navbar.component.ts
+++ src/app/admin/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from 'src/app/authentication/authentication.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  constructor(private authService: AuthenticationService) {}
+
+  // Logout method called when the logout button is clicked
+  logout(): void {
+    this.authService.logout();
+  }
 
 }
