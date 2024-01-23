8b2fceb875222b378ace1d010c69e46ec90be732
Aasthasree <sreelakshmi7032@gmail.com>
2024-01-23 12:25:47 +0530

    refactor(app-routing): update route configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
 import { CustomerListComponent } from './customer/customer-list/customer-list.component';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './admin/components/home/home.component';
 import { authGuard } from './authentication/authentication.guard';
 
 
 const routes: Routes = [
   {path: '',component: RegistrationComponent},
-  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
   {path: 'customer-service',component: CustomerListComponent},
-  {path: 'login',component: LoginComponent}
+  {path: 'login',component: LoginComponent,canActivate: [authGuard]},
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+    canActivate: [authGuard]
+  }
 ];
 
 @NgModule({
