b020d23430430e795175bb2aa88e6573f8a1dd8f
Aasthasree <sreelakshmi7032@gmail.com>
2024-01-23 12:27:06 +0530

    refactor(admin-routing): update route configuration

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,9 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ServicesComponent } from './components/services/services.component';
 
 const routes: Routes = [
+  {path:'',component:HomeComponent,
+children:[
+  {path:'',redirectTo:'/admin/home',pathMatch:'full'},
   { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'services', component: ServicesComponent },
+]},
 ];
 
 @NgModule({
