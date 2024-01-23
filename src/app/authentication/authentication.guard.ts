7d6dd8fcd65217823ee6d253163f93833609ebf0
Aasthasree <sreelakshmi7032@gmail.com>
2024-01-23 12:30:18 +0530

    refactor(auth-guard): update route guard logic

--- src/app/authentication/authentication.guard.ts
+++ src/app/authentication/authentication.guard.ts
@@ -5,16 +5,28 @@ import { AuthenticationService } from './authentication.service';
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthenticationService);
   const router = inject(Router);
+  console.log(localStorage['authToken'])
 
-  if (authService.isAuthenticatedUser()) {
-    // User is authenticated, allow access
+  if (localStorage['authToken']) {
+    // User is authenticated
+
+    if (state.url === '/login') {
+      // If the user is already authenticated and tries to access the login page,
+      router.navigate(['/admin']);
+    }
+    return true;
+  } else if (state.url !== '/login') {
+    // User is not authenticated and trying to access a route other than the login page,
+    router.navigate(['/login']);
+    return false;
+
+  } else {
+    // User is not authenticated but trying to access the login page
     return true;
   }
+};
 
-  // User is not authenticated, redirect to the login page
-  router.navigate(['/login']);
-  return false;
-}
+
 
 
 
