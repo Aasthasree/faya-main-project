fb3116e1469ea0c43b56a47194f142102a742d1f
Aasthasree <sreelakshmi7032@gmail.com>
2024-01-24 22:24:42 +0530

    refactor(customer): change customerData type to Customer

--- src/app/admin/components/customer/customer.component.ts
+++ src/app/admin/components/customer/customer.component.ts
@@ -2,6 +2,8 @@
 import { Component, OnInit } from '@angular/core';
 //Angular router imports
 import { ActivatedRoute } from '@angular/router';
+//modal
+import { Customer } from 'src/app/customer/customer-model/customer.model';
 //service
 import { CustomerService } from 'src/app/customer/service/customer.service';
 
@@ -11,7 +13,7 @@ import { CustomerService } from 'src/app/customer/service/customer.service';
   styleUrls: ['./customer.component.scss']
 })
 export class CustomerComponent implements OnInit{
-  customerData:any;
+  customerData:Customer;
 
   constructor(
     private activeRoute: ActivatedRoute,
