87213efe0a7e63fe38a87c002caa36bcb869bb80
Aasthasree <sreelakshmi7032@gmail.com>
2024-01-24 16:47:00 +0530

    refactor(home): update user interaction using click event

--- src/app/admin/components/home/home.component.ts
+++ src/app/admin/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 import { Customer } from 'src/app/customer/customer-model/customer.model';
 import { CustomerService } from 'src/app/customer/service/customer.service';
 
@@ -13,10 +14,22 @@ export class HomeComponent {
   selectedId: number;
   customerFormVisible: boolean;
 userId: any|string;
+  showParentComponent: boolean = true;
   
 
   constructor(
-    private customerService:CustomerService ,private router: Router  ) { }
+    private customerService:CustomerService ,private router: Router,
+    private activatedRoute:ActivatedRoute  ) {
+      // this.router.events.pipe(
+      //   filter(event => event instanceof NavigationEnd)
+      // ).subscribe(() => {
+      //   // Check if the current route has an 'id' parameter (child route)
+      //   const hasIdParameter = this.activatedRoute.snapshot.firstChild?.paramMap.has('id');
+      //   // Set the flag based on whether there's an 'id' parameter
+      //   this.showParentComponent = !hasIdParameter;
+      //   console.log(hasIdParameter,this.showParentComponent)
+      // });
+     }
 
   ngOnInit(): void {
     this.getCustomers();
@@ -33,6 +46,11 @@ userId: any|string;
         console.error('Error fetching data:', error);
       }
     );
+  }
+
+  onClickNavigate(id){
+    const url = this.router.url;
+    this.router.navigate([url,id]);
   }
 
 
