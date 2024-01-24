2e51277f11ead5494cc068fc434f1e90d453aad4
Aasthasree <sreelakshmi7032@gmail.com>
2024-01-24 16:42:51 +0530

    refactor(admin): update route configuration

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,19 +3,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 // Feature Components
-import { HomeComponent } from './components/home/home.component';
+// import { HomeComponent } from './components/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ServicesComponent } from './components/services/services.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { CustomerComponent } from './components/customer/customer.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path:'',component:AdminComponent,
-children:[
-  {path:'',redirectTo:'home',pathMatch:'full'},
-  { path: 'home', component: HomeComponent },
-  {path: 'home/customer/:id',component:CustomerComponent},
+  {path: '',component: AdminComponent,
+children: [
+  {path: '',redirectTo: 'home',pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'home/:id',  component: CustomerComponent},
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'services', component: ServicesComponent },
@@ -28,7 +29,7 @@ children:[
     CommonModule,
     RouterModule.forChild(routes)
   ],
-  exports:[
+  exports: [
     RouterModule
   ]
 })
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,13 +4,13 @@ import { CommonModule } from '@angular/common';
 //Feature Module
 import { AdminRoutingModule } from './admin-routing.module';
 //components
-import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ServicesComponent } from './components/services/services.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CustomerComponent } from './components/customer/customer.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 
