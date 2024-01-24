7977af70e9400ffbbc98893c22590d03823203bb
Aasthasree <sreelakshmi7032@gmail.com>
2024-01-24 09:26:58 +0530

    refactor(home): remove unwanted methods and implemented onClickEditCustomer method

--- src/app/admin/components/home/home.component.ts
+++ src/app/admin/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Customer } from 'src/app/customer/customer-model/customer.model';
 import { CustomerService } from 'src/app/customer/service/customer.service';
 
@@ -11,16 +12,26 @@ export class HomeComponent {
   customerList: Customer[]=[];
   selectedId: number;
   customerFormVisible: boolean;
+userId: any|string;
+  
 
   constructor(
-    private customerService:CustomerService
-     ) { }
+    private customerService:CustomerService ,private router: Router  ) { }
 
   ngOnInit(): void {
     this.getCustomers();
   }
 
-  //------To fetch data from customerService------
+  onClickEditCustomer(id: number): void {
+    // Handle the edit action with the id
+    console.log('Edit customer with id:', id);
+    // You can navigate to the edit form or perform any other actions as needed
+    
+
+    
+  }
+
+  // //------To fetch data from customerService------
   private getCustomers() {
     this.customerService.getCustomers().subscribe(
       (data) => {
@@ -32,44 +43,6 @@ export class HomeComponent {
     );
   }
 
-  /**
- * Toggles the visibility of the edit modal and updates the selected ID for editing.
- * @param isEdit - Indicates whether the modal is in edit mode.
- * @param id - (Optional) The identifier associated with the item being edited.
- */
-  onClickShowCustomerForm(id?: number): void {
-    this.selectedId = id || null;
-    this.customerFormVisible = true;
-  }
 
-  /**
- * Handles the click event to delete a row.
- * @param data - The data associated with the row to be deleted.
- */
-  onClickDeleteCustomer(data: Customer): void {
-    const index = this.customerList.indexOf(data);
-    if (index !== -1) {
-      this.customerList.splice(index, 1);
-      this.customerService.deleteCustomer(data.id).subscribe(
-        () => {
-          alert('Customer has been deleted successfully!');
-        },
-        (error) => {
-          console.error('Error deleting data from the database:', error);
-          this.customerList.splice(index, 0, data);
-        }
-      );
-    }
-  }
-
- /**
- * Conditionally triggers form update by loading data if the check is true.
- * @param check - A boolean flag determining whether to update the form.
- */
- updateCustomerListOnFormChange(check: boolean) {
-    if (check) {
-      this.getCustomers();
-    }
-  }
 }
 
