5f4e2ca3608e85354aaf206471d0ec16b2f254fd
Aasthasree <sreelakshmi7032@gmail.com>
2024-01-25 14:12:36 +0530

    refactor(login): organize imports

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,11 @@
-//Angular Core imports
-import { Component, Input, OnInit } from '@angular/core';
+//Angular imports
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+//validator
+import { CustomValidator } from '../shared/custom-validators/custom-validator';
 //service
 import { AuthenticationService } from './authservice/auth.service';
-//router
-import { Router } from '@angular/router';
-//Angular FormsModule
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { CustomValidator } from '../shared/custom-validators/custom-validator';
 
 @Component({
   selector: 'app-login',
@@ -14,16 +13,6 @@ import { CustomValidator } from '../shared/custom-validators/custom-validator';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  // Input property to receive registration data from the parent component
-  // @Input() regValue: any;
-  // // Property to store the concatenated password
-  // password: string = '';
-
-
-  // Lifecycle hook called after component initialization
-  // ngOnInit() {
-  //  this.password = this.regValue.First_name + this.regValue.Last_name;
-  //  }
 
   loginForm: FormGroup;
   username: string = '';
@@ -51,8 +40,10 @@ export class LoginComponent implements OnInit {
   }
 
   onClickSubmit(): void {
-    this.authenticationService.login(this.username, this.password);
-  }
+    const username = this.loginForm.get('username').value;
+    const password = this.loginForm.get('password').value;
+    this.authenticationService.login(username, password);
+}
 
 }
 
