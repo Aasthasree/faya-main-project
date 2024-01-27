f680090a2960036e3e3fc78d24377b1e8ff8502e
Aasthasree <sreelakshmi7032@gmail.com>
2024-01-27 21:31:11 +0530

    refactor(authguard): update authentication guard logic

--- src/app/login/authguard/authguard.ts
+++ src/app/login/authguard/authguard.ts
@@ -2,17 +2,27 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthenticationService } from '../authservice/auth.service';
 
+
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthenticationService);
   const router = inject(Router);
 
   if (localStorage['authToken']) {
     // User is authenticated
+
+    if (state.url === '/login') {
+      // If the user is already authenticated and tries to access the login page,
+      router.navigate(['/admin']);
+    }
     return true;
-  } else {
+  } else if (state.url !== '/login') {
     // User is not authenticated and trying to access a route other than the login page,
     router.navigate(['/login']);
     return false;
+
+  } else {
+    // User is not authenticated but trying to access the login page
+    return true;
   }
 };
 
