74d08e058a27a740b622b45141a887a48b565672
Aasthasree <sreelakshmi7032@gmail.com>
2024-01-29 10:26:49 +0530

    refactor(login): remove unwanted space

--- src/app/login/authguard/authguard.ts
+++ src/app/login/authguard/authguard.ts
@@ -2,8 +2,7 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
-
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = (_route, state) => {
   const router = inject(Router);
 
   if (localStorage['authToken']) {
