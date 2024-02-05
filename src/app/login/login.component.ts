583c7fd88ec4494ec85c904df748b799c75df228
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-05 09:29:25 +0530

    refactor(login): update login component with FormBuilder,subscription

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,12 @@
 //Angular imports
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 //validator
 import { CustomValidator } from '../shared/custom-validator/custom-validator';
 //service
 import { AuthenticationService } from './service/auth.service';
+
 
 @Component({
   selector: 'app-login',
@@ -12,27 +14,47 @@ import { AuthenticationService } from './service/auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
   loginForm: FormGroup;
+  errorMessage: string;
+
   constructor(
-    private authenticationService: AuthenticationService
+    private authenticationService: AuthenticationService,
+    private formBuilder: FormBuilder,
+    private router: Router
   ) { }
 
-  ngOnInit() {
-      this.initializeLoginForm();
+  ngOnInit(): void {
+    this.initializeLoginForm();
   }
 
-  initializeLoginForm(){
-    this.loginForm = new FormGroup({
-      'username': new FormControl('', [Validators.required, CustomValidator.cannotContainSpace]),
-      'password': new FormControl('', [Validators.required, CustomValidator.cannotContainSpace])
+  initializeLoginForm(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required, CustomValidator.cannotContainSpace],
+      password: ['', Validators.required, CustomValidator.cannotContainSpace]
     });
   }
 
   onClickSubmit(): void {
     const username = this.loginForm.get('username').value;
     const password = this.loginForm.get('password').value;
-    this.authenticationService.login(username, password);
-}
+    const grant_type = 'password';
+
+    this.authenticationService.login({ grant_type, username, password })
+    .subscribe({
+      next: (response) => {
+        if (response.access_token){
+          console.log('Login successful:', response);
+          localStorage.setItem('token',response.access_token);
+          this.router.navigate(['/admin']);
+        }
+
+       },
+      error: (error) => {
+        console.error( error);
+       },
+  });
+   
+
+  }
 
 }
