91289c8e75033cb3ad2b2202ffa02fa557bc78e2
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-05 12:22:56 +0530

    refactor(core): resolve eslint errors

--- src/app/shared/interceptor/interceptor.ts
+++ src/app/shared/interceptor/interceptor.ts
@@ -14,16 +14,16 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private authenticationService: AuthenticationService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-   
-    const authToken = this.authenticationService.getToken();
-    console.log(authToken)
-    
 
-    if(authToken){
+    const authToken = this.authenticationService.getToken();
+    console.log(authToken);
+
+
+    if (authToken){
       request = request.clone({
         setHeaders: {Authorization: `Bearer ${authToken}`}
-      })
-      
+      });
+
     }
     return next.handle(request);
   }
