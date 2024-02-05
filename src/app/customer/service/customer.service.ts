c79b7fa39960eabfaf950aa6a4a764b2e7e59495
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-05 10:26:28 +0530

    refactor(customer-service): update api url and comment out unused codes

--- src/app/customer/service/customer.service.ts
+++ src/app/customer/service/customer.service.ts
@@ -1,19 +1,19 @@
 //Angular imports
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable, SkipSelf } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 // RxJS imports
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 //interface
-import { Customer } from '../customer-model/customer.model';
+import { CustomerResponse } from '../customer-model/customer.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CustomerService {
 
-  private apiUrl = 'https://retoolapi.dev/TMekp1/data';
+  private apiUrl = 'https://pod6.salesonepro.com:5001/api/e/customers';
 
   constructor(
     private http: HttpClient,
@@ -21,44 +21,44 @@ export class CustomerService {
     ) { }
 
   // -------------Service method to fetch data from the API endpoint---------------------
-  getCustomers(): Observable<Customer[]> {
-    return this.http.get<Customer[]>(this.apiUrl).pipe(
+  getCustomers(): Observable<CustomerResponse> {
+    return this.http.get<CustomerResponse>(this.apiUrl).pipe(
       catchError(this.handleError.bind(this))
     );
   }
 
-  //-------------------------Fetches a single data item from the API endpoint by its identifier--------------------
-  getCustomer(id): Observable<Customer> {
-    const url = `${this.apiUrl}/${id}`;
-    return this.http.get<Customer>(url).pipe(
-      catchError(this.handleError.bind(this))
-    );
-  }
+//   //-------------------------Fetches a single data item from the API endpoint by its identifier--------------------
+//   getCustomer(id): Observable<Customer> {
+//     const url = `${this.apiUrl}/${id}`;
+//     return this.http.get<Customer>(url).pipe(
+//       catchError(this.handleError.bind(this))
+//     );
+//   }
 
-  //------------------- Deletes a data item from the API endpoint by its identifier.-------------------
-  deleteCustomer(id: number): Observable<Customer> {
-    const url = `${this.apiUrl}/${id}`;
-    return this.http.delete<Customer>(url).pipe(
-      catchError(this.handleError.bind(this))
-    );
-  }
+//   //------------------- Deletes a data item from the API endpoint by its identifier.-------------------
+//   deleteCustomer(id: number): Observable<Customer> {
+//     const url = `${this.apiUrl}/${id}`;
+//     return this.http.delete<Customer>(url).pipe(
+//       catchError(this.handleError.bind(this))
+//     );
+//   }
 
-  //-------------------Updates a data item at the API endpoint by its identifier.----------------------
-  updateCustomerById(id: number, value: Customer): Observable<Customer> {
-    const url = `${this.apiUrl}/${id}`;
-    return this.http.patch<Customer>(url, value).pipe((map((data) => {
-      return data;
-    })));
-  }
+//   //-------------------Updates a data item at the API endpoint by its identifier.----------------------
+//   updateCustomerById(id: number, value: Customer): Observable<Customer> {
+//     const url = `${this.apiUrl}/${id}`;
+//     return this.http.patch<Customer>(url, value).pipe((map((data) => {
+//       return data;
+//     })));
+//   }
 
-  //-------------------------Creates a new data item by sending an HTTP POST request to the API endpoint.---------------
-  createCustomer(value: Customer): Observable<Customer> {
-    return this.http.post<Customer>(this.apiUrl, value).pipe((map((data) => {
-      return data;
-    })));
-  }
+//   //-------------------------Creates a new data item by sending an HTTP POST request to the API endpoint.---------------
+//   createCustomer(value: Customer): Observable<Customer> {
+//     return this.http.post<Customer>(this.apiUrl, value).pipe((map((data) => {
+//       return data;
+//     })));
+//   }
 
-  //-------Handles API errors uniformly-----
+//   //-------Handles API errors uniformly-----
   private handleError(error: HttpErrorResponse) {
     if (error.status === 404) {
       console.error('Resource not found:', error);
@@ -72,6 +72,6 @@ export class CustomerService {
       return throwError('Something went wrong; please try again later.');
     }
   }
+// }
+
 }
-
-
