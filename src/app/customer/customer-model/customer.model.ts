aa0edb212fedc7ab983e5faf395c534f4ae2139c
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-05 10:29:13 +0530

    feat(customer-model): add customerResponse interface

--- src/app/customer/customer-model/customer.model.ts
+++ src/app/customer/customer-model/customer.model.ts
@@ -1,10 +1,21 @@
-export interface Customer {
-  id: number;
-  dob: string;
-  email: string;
-  Skills: string[];
-  f_name: string;
-  gender: string;
-  l_name: string;
-  phone_number: number;
+interface Customer {
+  default_contact: {
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone: number | null;
+
+  };
+  address: {
+    country: string;
+    state: string;
+  }
+  id: string;
+}
+
+export interface CustomerResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Customer[]
 }
