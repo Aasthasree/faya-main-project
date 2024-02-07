07eb7d2ac97bd70e3abce463bd79e512868ef5b9
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-07 12:02:05 +0530

    refactor(app-routing): remove routes and imports of unused components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,26 +3,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 //component
 import { RegistrationComponent } from './registration/registration.component';
-import { CustomerListComponent } from './customer/customer-list/customer-list.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 //Authguard
 import { authGuard } from './login/authguard/authguard';
 
 
-
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'registration', component: RegistrationComponent},
-  {path: 'customer-service', component: CustomerListComponent},
-  {path: 'login', component: LoginComponent, canActivate: [authGuard]},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent, canActivate: [authGuard] },
   {
     path: 'admin',
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
     canActivate: [authGuard]
   },
-  {path: 'not-found', component: NotFoundComponent},
-  {path: '**', redirectTo: 'not-found'}
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: 'not-found' }
 
 ];
 
