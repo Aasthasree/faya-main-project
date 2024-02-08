3e7ce3d22279c421f0051510d588f0881b3b2650
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-08 18:55:02 +0530

    refactor(interceptor): remove token refresh logic

--- src/app/login/interceptor/interceptor.ts
+++ src/app/login/interceptor/interceptor.ts
@@ -7,7 +7,7 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 //RXJS imports
-import { Observable, catchError, switchMap, throwError } from 'rxjs';
+import { Observable} from 'rxjs';
 //Service
 import { AuthenticationService } from 'src/app/login/service/authentication';
 
@@ -28,26 +28,7 @@ export class Interceptor implements HttpInterceptor {
       request = this.addTokenHeader(request, authToken);
     }
 
-    return next.handle(request).pipe(
-      catchError(errordata => {
-        console.log(errordata.status);
-        if (errordata.status === 403 || errordata.status === 401) {
-          return this.handleRefreshToken(request, next);
-        }
-        return throwError(() => errordata);
-      })
-    );
-  }
-
-  handleRefreshToken(request: HttpRequest<unknown>, next: HttpHandler) {
-    return this.authenticationService.refreshAccessToken().pipe(
-      switchMap((response) => {
-        const newToken = response.access_token;
-        this.authenticationService.setToken(response);
-        const newRequest = this.addTokenHeader(request, newToken);
-        return next.handle(newRequest);
-      })
-    );
+    return next.handle(request);
   }
 
   addTokenHeader(request: HttpRequest<unknown>, token: string, refresh?) {
